f4f8a78a6d93fc90846ad8f418f0b342a61055a5
Thura Freelance <mozet89@gmail.com>
2023-10-20 09:05:16 +0630

    uploading counter challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [step, setStep] = useState(1);
+  const [counter, setCounter] = useState(0);
+  const stepDecrease = () => {
+    setStep(step - 1);
+  };
+  const stepIncrease = () => {
+    setStep(step + 1);
+  };
+  const counterDecrease = () => {
+    setCounter(Number(counter) - step);
+  };
+  const counterIncrease = () => {
+    setCounter(Number(counter) + step);
+  };
+
+  const date = new Date();
+  date.setDate(date.getDate() + counter);
+  const resetHandling = () => {
+    setCounter(0);
+    setStep(1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div className="d-flex gap-1 justify-content-center mt-5">
+        <input
+          type="range"
+          className="custom-range"
+          min="1"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        {/* <button
+          type="button"
+          class="btn btn-info"
+          onClick={() => stepDecrease()}
         >
-          Learn React
-        </a>
-      </header>
+          -
+        </button> */}
+        <h2>Step : {step}</h2>
+
+        {/* <button
+          type="button"
+          class="btn btn-warning"
+          onClick={() => stepIncrease()}
+        >
+          +
+        </button> */}
+      </div>
+      <div className="d-flex gap-1 justify-content-center mt-5">
+        <button
+          type="button"
+          class="btn btn-info"
+          onClick={() => counterDecrease()}
+        >
+          -
+        </button>
+        {/* <h2>Counter : {counter}</h2> */}
+        <input
+          value={counter}
+          type="text"
+          placeholder="Counter"
+          onChange={(e) => setCounter(Number(e.target.value))}
+        />
+        <button
+          type="button"
+          class="btn btn-warning"
+          onClick={() => counterIncrease()}
+        >
+          +
+        </button>
+      </div>
+      <div className="text-center mt-5">
+        <h1>
+          {counter === 0
+            ? "Today is "
+            : counter > 0
+            ? `${counter} days from today is `
+            : `${Math.abs(counter)} days ago was `}
+          {date.toDateString()}
+        </h1>
+      </div>
+      {counter !== 0 || step !== 1 ? (
+        <div className="text-center mt-5">
+          <button
+            className="btn btn-danger d-inline"
+            onClick={() => resetHandling()}
+          >
+            Reset
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
-}
+};
 
 export default App;
